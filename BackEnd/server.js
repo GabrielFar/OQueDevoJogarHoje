6d2675b4d656b8bac3146fbead5c1ddededbd45c
Gabriel Farina <gabriel.farina2358@gmail.com>
2025-12-09 20:10:56 -0300

    adicionado cors ao servidor e adicionado front inicial

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,15 @@
+const cors = require("cors");
 const express = require("express");
 const jogosRouter = require("./controllers/jogo_controller");
 const generosRouter = require("./controllers/genero_controller");
 const plataformasRouter = require("./controllers/plataforma_controller");
-
 const path = require('path');
-app.use('/imagens', express.static(path.join(__dirname, 'imagens')));
+
 const app = express();
+
+app.use('/imagens', express.static(path.join(__dirname, 'imagens')));
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3002;
 app.listen(PORT, () => console.log(`Servidor está rodando na porta ${PORT}.`));
